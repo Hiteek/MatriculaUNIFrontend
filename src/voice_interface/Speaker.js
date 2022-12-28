4423ff73771eea2b3d846a84fb1de593e7f60bf7
Cegax123 <emanuel5552011@hotmail.com>
2022-12-28 11:47:44 -0500

    speaker

---
+++ src/voice_interface/Speaker.js
@@ -0,0 +1,12 @@
+
+export default function Speaker(text) {
+  const synth = window.speechSynthesis;
+  const voices = synth.getVoices();
+
+  const msg = new SpeechSynthesisUtterance(text)
+  msg.lang = 'es-MX';
+  msg.voice = voices[66]
+
+  synth.speak(msg);
+}
+
